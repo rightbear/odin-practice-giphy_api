2223306086b2a3ba09c412a7f7c315a0137491fb
rightbear <jjj17041113@gmail.com>
2025-06-25 17:01:54 +0800

    feat: finish basic function to show a image on the page

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import "./styles.css";
 import { greeting } from "./greeting.js";
 
 console.log(greeting);
+
+const img = document.querySelector('img');
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=QpLVtnUCvKkMTkPJVPxYtd8RvZ0XXgmR&s=cats', {mode: 'cors'})
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      img.src = response.data.images.original.url;
+    });
