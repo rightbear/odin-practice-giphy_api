9bc64580857bf118ed8761b74bfd7396c9b831f5
rightbear <jjj17041113@gmail.com>
2025-06-26 15:57:58 +0800

    feat: add function of getting GIF when clicking the cat button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import "./styles.css";
+import noGIF from "./images/No-GIF.jpg";
+import connectError from "./images/Connect-Error.jpg";
 
 const img = document.querySelector('img');
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=QpLVtnUCvKkMTkPJVPxYtd8RvZ0XXgmR&s=cats', {mode: 'cors'})
+const input = document.querySelector('input');
+const searchBtn = document.querySelector('#searchBtn');
+const catBtn = document.querySelector('#catBtn');
+const myKey = "QpLVtnUCvKkMTkPJVPxYtd8RvZ0XXgmR";
+
+catBtn.addEventListener('click', function showCatGIF(){
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${myKey}&s=cats`, {mode: 'cors'})
     .then(function(response) {
       return response.json();
     })
     .then(function(response) {
       img.src = response.data.images.original.url;
+      setTimeout(() => {alert('You successfully summon some cats!')}, 500);
     })
     .catch(error => {
       console.log(error);
     });
+});
