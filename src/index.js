2adbf292582204971fe6b425871c942149324798
rightbear <jjj17041113@gmail.com>
2025-06-28 00:09:08 +0800

    refactor: convert the promise based code of search button into async/await compatible code

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const goodSmileEmoji = "\u{1F604}";
 const pensiveEmoji = "\u{1F614}";
 
 // Show the requested GIF with input keywords
-searchBtn.addEventListener("click", function showNewGIF() {
+searchBtn.addEventListener("click", async function showNewGIF() {
   // Check whether the input keyword exists when searching
   if (input.value === "") {
     alert(
@@ -27,61 +27,77 @@ searchBtn.addEventListener("click", function showNewGIF() {
     searchParams.append("api_key", `${myKey}`);
     searchParams.append("s", `${inputKeyword}`);
 
-    fetch(`https://api.giphy.com/v1/gifs/translate?${searchParams}`, {
-      mode: "cors",
-    })
-      .then(function (response) {
-        // Check whether the connection to the GIF website is successful
-        if (!response.ok) {
-          // response.ok is true when status code is 200~299
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(function (response) {
-        // Check whether there is corresponding GIF to the input keyword.
-        // If the keyword has no corresponding GIF, the website still returns status code 200
-        // But the length of data array in response json file will be 0, we can show some result based on it
-        if (response.data.length === 0) {
-          img.src = noGIF;
-          setTimeout(() => {
+    try {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/translate?${searchParams}`,
+        { mode: "cors" },
+      );
+      // Check whether the connection to the GIF website is successful
+      if (!response.ok) {
+        // response.ok is true when status code is 200~299
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const gifData = await response.json();
+      /*
+       * Check whether there is corresponding GIF to the input keyword.
+       * If the keyword has no corresponding GIF, the website still returns status code 200
+       * But the length of data array in response json file will be 0, we can show some result based on it
+       */
+      if (gifData.data.length === 0) {
+        img.src = noGIF;
+        setTimeout(
+          () =>
             alert(
               `No result for the input keyword. Maybe change a new keyword? ${confuseEmoji}`,
-            );
-          }, 1000);
-        }
-        // If the keyword has corresponding GIFs, show the returned result.
-        else {
-          img.src = response.data.images.original.url;
-          setTimeout(() => {
-            alert(`Here is the GIF you want. Enjoy it! ${goodSmileEmoji}`);
-          }, 2000);
-        }
-      })
-      .catch((error) => {
-        // If there is some error thrown out when requesting the GIF, show the error message
-        img.src = connectError;
-        console.log(error);
+            ),
+          1000,
+        );
+      }
+      // If the keyword has corresponding GIFs, show the returned result.
+      else {
+        img.src = gifData.data.images.original.url;
         setTimeout(() => {
+          alert(`Here is the GIF you want. Enjoy it! ${goodSmileEmoji}`);
+        }, 2000);
+      }
+    } catch (error) {
+      // If there is some error thrown out when requesting the GIF, show the error message
+      img.src = connectError;
+      console.log(error);
+      setTimeout(
+        () =>
           alert(
             `Oops! There is some trouble here. Please try to search later. ${pensiveEmoji}`,
-          );
-        }, 1000);
-      });
+          ),
+        1000,
+      );
+    }
   }
 });
 
 // Show the requested GIF about cats
 catBtn.addEventListener("click", async function showCatGIF() {
   try {
-    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${myKey}&s=cats`, {mode: "cors"});
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=${myKey}&s=cats`,
+      { mode: "cors" },
+    );
     const catData = await response.json();
     img.src = catData.data.images.original.url;
-    setTimeout(() => alert(`You successfully summon little cute cats! ${catEmoji}`), 2000);
-  } catch(error) {
+    setTimeout(
+      () => alert(`You successfully summon little cute cats! ${catEmoji}`),
+      2000,
+    );
+  } catch (error) {
     // If there is some error thrown out when requesting the GIF, show the error message
     img.src = connectError;
     console.log(error);
-    setTimeout(() => alert(`Oops! There is some trouble here. Please try to search later. ${pensiveEmoji}`), 1000);
+    setTimeout(
+      () =>
+        alert(
+          `Oops! There is some trouble here. Please try to search later. ${pensiveEmoji}`,
+        ),
+      1000,
+    );
   }
 });
